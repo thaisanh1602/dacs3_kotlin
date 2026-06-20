d65f9f1c8c1357cb36f14276963ff918162a2c42
Anh1hpzz <thaisanh160206@gmail.com>
2026-06-20 07:45:49 +0700

    fix

--- app/src/main/java/com/example/angrismart/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/angrismart/data/remote/RetrofitClient.kt
@@ -5,8 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     // Thay bằng IP mạng LAN của máy tính (ví dụ 192.168.x.x) nếu chạy điện thoại thật
-    private const val BASE_URL = "http://10.0.2.2:8000/api/v1/"
-
+    private const val BASE_URL = "http://172.26.59.204:8000/api/v1/"
     val apiService: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
